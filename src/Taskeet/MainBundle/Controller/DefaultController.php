1e01e991b1fd61859038eb38648bda07bf09819a
Rafael Ernesto Ferro <rafix81@gmail.com>
2013-06-12 20:39:39 -0400

    gráfica de estados de las tareas del proyecto

--- src/Taskeet/MainBundle/Controller/DefaultController.php
+++ src/Taskeet/MainBundle/Controller/DefaultController.php
@@ -3,12 +3,35 @@
 namespace Taskeet\MainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Ob\HighchartsBundle\Highcharts\Highchart;
 
 class DefaultController extends Controller
 {
     public function welcomeAction()
     {
+        $ob = new Highchart();
+        $ob->chart->renderTo('linechart');
+        $ob->title->text('Browser market shares at a specific website in 2010');
+        $ob->plotOptions->pie(array(
+            'allowPointSelect'  => true,
+            'cursor'    => 'pointer',
+            'dataLabels'    => array('enabled' => false),
+            'showInLegend'  => true
+        ));
+        $data = array(
+            array('Firefox', 45.0),
+            array('IE', 26.8),
+            array('Chrome', 12.8),
+            array('Safari', 8.5),
+            array('Opera', 6.2),
+            array('Others', 0.7),
+        );
+        $ob->series(array(array('type' => 'pie','name' => 'Browser share', 'data' => $data)));
+
         return $this->render('TaskeetMainBundle:Dashboard:welcome.html.twig',
-                                array('User' => $this->getUser()));
+                                array(
+                                    'User' => $this->getUser(),
+                                    'chart' => $ob
+                                ));
     }
 }
--- src/Taskeet/MainBundle/Controller/Project/ShowController.php
+++ src/Taskeet/MainBundle/Controller/Project/ShowController.php
@@ -3,7 +3,40 @@
 namespace Taskeet\MainBundle\Controller\Project;
 
 use Admingenerated\TaskeetMainBundle\BaseProjectController\ShowController as BaseShowController;
+use Ob\HighchartsBundle\Highcharts\Highchart;
 
 class ShowController extends BaseShowController
 {
+    public function chartAction($pk)
+    {
+        $Project = $this->getObject($pk);
+
+        $this->checkCredentials($Project);
+
+
+        if (!$Project) {
+            throw new NotFoundHttpException("The Taskeet\MainBundle\Entity\Project with id $pk can't be found");
+        }
+
+        $ob = new Highchart();
+        $ob->chart->renderTo('linechart');
+        $ob->title->text($Project->getName());
+        $ob->plotOptions->pie(array(
+            'allowPointSelect'  => true,
+            'cursor'    => 'pointer',
+            'dataLabels'    => array('enabled' => false),
+            'showInLegend'  => true
+        ));
+        $data = array();
+        foreach ($Project->getTickets() as $task)
+        {
+            $data[] = array($task->getStatus()->getName(), +1);
+        }
+//        $g = array($data);
+        $ob->series(array(array('type' => 'pie','name' => 'Estado de las tareas', 'data' => $data)));
+
+        return $this->render('TaskeetMainBundle:ProjectShow:chart.html.twig', $this->getAdditionalRenderParameters($Project) + array(
+            "Project" => $Project, 'chart' => $ob
+        ));
+    }
 }
